5d7ebbf68bd4c536a6f6b0827d97337a60cb5034
Tarik <tarikmerrylees@gmail.com>
2022-08-01 10:47:03 +0100

    Initial Commit

---
+++ Shared/ChatBotTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotTutorialApp.swift
+//  Shared
+//
+//  Created by Tarik Merrylees on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
